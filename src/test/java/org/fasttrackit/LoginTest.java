9a10731b30c0d4bf1d1d6957ba018ea33f2e7763
cosminciocan <ciocan.cosmin@gmail.com>
2019-07-26 12:43:23 +0300

    Lab 17

--- src/test/java/org/fasttrackit/LoginTest.java
+++ src/test/java/org/fasttrackit/LoginTest.java
@@ -1,6 +1,6 @@
 package org.fasttrackit;
 
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,10 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
+
+
+   private static WebDriver driver;
+
+    @BeforeClass
+    public static void initDriver(){
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
     @Test
-    public void validLoginTest(){
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void validLoginTest() {
+
         driver.get("https://fasttrackit.org/selenium-test/");
         WebElement accountButton = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
         accountButton.click();
@@ -22,15 +33,19 @@ public class LoginTest {
         driver.findElement(By.cssSelector("#email")).sendKeys("cosmin@fasttrackit.org");
         driver.findElement(By.cssSelector("#pass")).sendKeys("123456");
         driver.findElement(By.cssSelector("#send2")).click();
+        WebElement helloText = driver.findElement(By.cssSelector(".welcome-msg .hello"));
 
+        Assert.assertEquals("Hello, asdasda asdasd!", helloText.getText());
 
-        driver.quit();
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("a[title='Log Out']")).isDisplayed());
+
     }
 
     @Test
-    public void negativeLogin(){
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void negativeLogin() {
+
         driver.get("https://fasttrackit.org/selenium-test/");
         WebElement accountButton = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
         accountButton.click();
@@ -43,6 +58,10 @@ public class LoginTest {
         driver.findElement(By.cssSelector("#send2")).click();
 
 
+    }
+
+    @AfterClass
+    public static void quit(){
         driver.quit();
     }
 }
